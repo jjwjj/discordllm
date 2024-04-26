ba38ec86a2fafe27ea2559cb3e555c29ff6c97c0
jjwjj <jjw@jjwjj.com>
2024-04-26 12:44:26 +0000

    added placeholders for more api keys

--- credentials_template.py
+++ credentials_template.py
@@ -13,6 +13,13 @@ import os
 
 openaikey = "sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"  # Your OpenAI key
 discord_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx" # Your Discord Token
+googleaikey    = 'AIxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+tavilyaikey    = 'tvly-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+fireworksaikey = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+anthropicaikey = 'sk-xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+huggingfacekey = 'hf_xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+exaapikey      = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+mistralkey     = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 
 
@@ -27,7 +34,7 @@ discord_token = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx
 #    ##     ## ####       ##     ##  #######  ########  ######## ########  ######  
 
 
-aimodel  = 'gpt-4-turbo-preview'
+aimodel  = 'gpt-4-turbo'
 embedmodel = "text-embedding-ada-002"
 
 
@@ -43,7 +50,15 @@ embedmodel = "text-embedding-ada-002"
 #     ##       ##   ###   ## ##              ## ##   ##     ## ##    ##  ##    ## 
 #     ######## ##    ##    ###                ###    ##     ## ##     ##  ######       
                                                             
-os.environ['OPENAI_API_KEY'] = openaikey 
+os.environ['OPENAI_API_KEY']           = openaikey 
+os.environ['TAVILY_API_KEY']           = tavilyaikey  
+os.environ["FIREWORKS_API_KEY"]        = fireworksaikey
+os.environ['GOOGLE_API_KEY']           = googleaikey
+os.environ['ANTHROPIC_API_KEY']        = anthropicaikey
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = huggingfacekey
+os.environ['MISTRAL_API_KEY']          = mistralkey
+os.environ['EXA_API_KEY']              = exaapikey
+os.environ['OPENAI_MODEL_NAME']        = aimodel
 
 
 
